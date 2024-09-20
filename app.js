67e851948b27a8188a6870c134034ab1f20e8841
Noor Elahi <shamsinoorelahi@gmail.com>
2024-09-20 17:38:25 +0500

    assignmet 15

---
+++ app.js
@@ -0,0 +1,87 @@
+//qns no 2
+
+// function compareNumbers(num1, num2) {
+//     if (num1 > num2) {
+//         return `${num1} is larger.`;
+//     } else if (num2 > num1) {
+//         return `${num2} is larger.`;
+//     } else {
+//         return `Both numbers are equal.`;
+//     }
+// }
+
+
+// let input1 = parseInt(prompt("Enter the first integer:"));
+// let input2 = parseInt(prompt("Enter the second integer:"));
+
+// let result = compareNumbers(input1, input2);
+// console.log(result);
+
+
+
+//qns no 3
+// function checkNumber(num) {
+//     if (num > 0) {
+//         return "The number is positive.";
+//     } else if (num < 0) {
+//         return "The number is negative.";
+//     } else {
+//         return "The number is zero.";
+//     }
+// }
+
+
+// let userInput = parseFloat(prompt("Enter a number:"));
+// let result = checkNumber(userInput);
+// console.log(result);
+
+//qns no 4
+
+// function isVowel(character) {
+   
+//     let lowerChar = character.toLowerCase();
+    
+
+//     return ['a', 'e', 'i', 'o', 'u'].includes(lowerChar);
+// }
+
+// let userInput = prompt("Enter a character:");
+// if (userInput.length === 1) {
+//     let result = isVowel(userInput);
+//     console.log(result);
+// } else {
+//     console.log("Please enter a single character.");
+// }
+
+
+//qns no 5
+
+
+// let correctPassword = "mypassword123";
+
+// let  userInput = prompt("Please enter your password:");
+
+
+// if (!userInput) {
+  
+//     console.log("Please enter your password.");
+// } else if (userInput === correctPassword) {
+   
+//     console.log("Correct! The password you entered matches the original password.");
+// } else {
+//     console.log("Incorrect password.");
+// }
+
+
+//qns no 6
+
+// This if/else statement does not work. Try to fix it:
+// var greeting;
+// var hour = 19;
+// if (hour < 18) {
+// greeting = "Good day";}
+// else{
+// greeting = "Good evening";
+// }
+
+// console.log(greeting);
